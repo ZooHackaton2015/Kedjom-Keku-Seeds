4707fec451d1562525498ad648e7f948735bcb7f
David Votrubec <votrubec.d@st-software.com>
2015-04-12 12:09:43 +0200

    map location auto updates

--- web/ui/js/services/services.js
+++ web/ui/js/services/services.js
@@ -3,14 +3,18 @@
 myApp.factory('config', function() {
     return {
         baseUrl: 'https://www.googleapis.com/fusiontables/v2/',
-        apiKey: ''
+        apiKey: 'AIzaSyDifK82ApYObrQZJRXmJp6nxqTem6yjrlY'
     }
 })
 
-myApp.factory('areasResourceService', function(config, $resource) {    
+myApp.factory('areasResourceService', function(config, $resource) {   
+    var tableId = '16zyilWrVedmB0D7WkFulJxPjxb1Fav3tZsNdN2dK';
+    
     var res = $resource(config.baseUrl, {
         //expecting someting like https://www.googleapis.com/fusiontables/v2/query?sql=SELECT * FROM 1KxVV0wQXhxhMScSDuqr-0Ebf0YEt4m4xzVplKd4&
-        query: {method: 'GET', url: config.baseUrl + 'query'}
+        query: {method: 'GET', url: config.baseUrl + 'query'},
+        saveNew: {method: 'POST', url: config.baseUrl + 'insert'},
+        save: {method: 'POST', url: config.baseUrl + 'update'}
     });
     return res;    
 })
